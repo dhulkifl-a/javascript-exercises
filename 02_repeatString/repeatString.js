de9cf99951d9aa574e34209d0d539615a3662c09
dahmed <DhulkiflA@Gmail.com>
2022-12-26 17:28:29 +1300

    Add if statment for the scenario when num is negative

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,10 +2,14 @@ const repeatString = function(string, num) {
 let newString = '';
 let i = 0;
 
+if (num <0) {
+    return "ERROR"
+} else{
+
 for (; i < num; i++) {
     newString += string
 }
-
+}
 return newString
 
 };
