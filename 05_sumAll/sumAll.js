fa75af57a6248b14e4b27e0e0e615695fedb68e2
dahmed <DhulkiflA@Gmail.com>
2022-12-27 19:39:44 +1300

    Create the sum function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(i,j) {
 
+    let sum = 0;
+
+    for (; i < j; i++) {
+        sum += i;
+    }
+
+    return sum
 };
 
 // Do not edit below this line
