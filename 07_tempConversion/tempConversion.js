02e8181e7f1e0ec9278a828c0275ffa364339f3d
dahmed <DhulkiflA@Gmail.com>
2022-12-27 20:59:36 +1300

    Complete the task

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let newTempC = (tempF - 32) * 5/9;
+  return Math.round(newTempC)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let newTempF = tempc * 9/5 + 32;
+  return Math.round(newTempC)
 };
 
 // Do not edit below this line
