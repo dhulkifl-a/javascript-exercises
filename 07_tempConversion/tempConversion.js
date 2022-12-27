b246dd734dd409dfcf36ea76a2c6ab32adf4791d
dahmed <DhulkiflA@Gmail.com>
2022-12-27 21:18:25 +1300

    Fix math.round to math.round(num * 10) / 10

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const convertToCelsius = function(tempF) {
-  let newTempC = (tempF - 32) * 5/9;
-  return Math.round(newTempC,1)
+  let newTempC = (tempF - 32) * (5/9);
+  return Math.round(newTempC * 10) / 10
 };
 
 const convertToFahrenheit = function(tempC) {
-  let newTempF = tempc * 9/5 + 32;
-  return Math.round(newTempC,1)
+  let newTempF = tempC * 9/5 + 32;
+  return Math.round(newTempF * 10) / 10
 };
 
 // Do not edit below this line
