ebf5afe25bd4ade68141df04b6c98614daf1c4cf
dahmed <DhulkiflA@Gmail.com>
2022-12-27 21:09:17 +1300

    Edit math.round so it gives returns answers as 1dp

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const convertToCelsius = function(tempF) {
   let newTempC = (tempF - 32) * 5/9;
-  return Math.round(newTempC)
+  return Math.round(newTempC,1)
 };
 
 const convertToFahrenheit = function(tempC) {
   let newTempF = tempc * 9/5 + 32;
-  return Math.round(newTempC)
+  return Math.round(newTempC,1)
 };
 
 // Do not edit below this line
